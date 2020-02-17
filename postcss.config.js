5ed2cf945326a99faf6748d3d8b2fe25c4a6105c
Resta Adiputra <resta.adiputra@myboost.id>
2020-02-17 21:38:18 +0700

    Add property section with basic card

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,9 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    require('@fullhuman/postcss-purgecss')({
+    process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
       content: [
+        './src/**/*.vue',
         './public/index.html',
       ],
       defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
